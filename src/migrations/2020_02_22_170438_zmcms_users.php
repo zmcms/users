4e18e03817080a5706f8b5cdfe36b4142de64c18
zmcms <59611848+zmcms@users.noreply.github.com>
2020-02-23 23:17:30 +0100

    Update 2020_02_22_170438_zmcms_users.php

--- src/migrations/2020_02_22_170438_zmcms_users.php
+++ src/migrations/2020_02_22_170438_zmcms_users.php
@@ -19,12 +19,10 @@ class ZmcmsUsers extends Migration{
 		Schema::table($tblName, function($table){$table->enum('sex', ['M','W', 'NS']);}); //M- man W-Woman NS - not specified
 		Schema::table($tblName, function($table){$table->string('ilustration', 255)->nullable();});
 		Schema::table($tblName, function($table){$table->date('birthday')->nullable();}); // Data urodzin
-		Schema::table($tblName, function($table){$table->string('active', 1);});
 		Schema::table($tblName, function($table){$table->string('confirmed', 1);});
 		Schema::table($tblName, function($table){$table->tinyInteger('login_try')->default(Config('zmcms.frontend_users.max_login_try'));});
 		Schema::table($tblName, function($table){$table->tinyInteger('total_login_try')->default(Config('zmcms.frontend_users.max_total_login_try'));});		
 		Schema::table($tblName, function($table){$table->string('homepage', 70)->default(Config('zmcms.frontend_users.default_home_page'));});
-		Schema::table($tblName, function($table){$table->string('homepage', 70)->default(Config('zmcms.frontend_users.default_login_page'));});
 		Schema::table($tblName, function($table){$table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));});//Imię
 		Schema::table($tblName, function($table){$table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP'));});//Imię
 		Schema::table($tblName, function($table){$table->primary('token');});
@@ -42,12 +40,10 @@ class ZmcmsUsers extends Migration{
 		Schema::table($tblName, function($table){$table->enum('sex', ['M','W', 'NS']);}); //M- man W-Woman NS - not specified
 		Schema::table($tblName, function($table){$table->string('ilustration', 255)->nullable();});
 		Schema::table($tblName, function($table){$table->date('birthday')->nullable();}); // Data urodzin
-		Schema::table($tblName, function($table){$table->string('active', 1);});
 		Schema::table($tblName, function($table){$table->string('confirmed', 1);});
 		Schema::table($tblName, function($table){$table->tinyInteger('login_try')->default(Config('zmcms.backend_users.max_login_try'));});
 		Schema::table($tblName, function($table){$table->tinyInteger('total_login_try')->default(Config('zmcms.backend_users.max_total_login_try'));});		
 		Schema::table($tblName, function($table){$table->string('homepage', 70)->default(Config('zmcms.backend_users.default_home_page'));});
-		Schema::table($tblName, function($table){$table->string('homepage', 70)->default(Config('zmcms.backend_users.default_login_page'));});
 		Schema::table($tblName, function($table){$table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));});//Imię
 		Schema::table($tblName, function($table){$table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP'));});//Imię
 		Schema::table($tblName, function($table){$table->primary('token');});
