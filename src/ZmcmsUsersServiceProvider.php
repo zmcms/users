ea08da7d58424649260f508d87156cc0f0e39932
zmcms <59611848+zmcms@users.noreply.github.com>
2020-02-23 21:38:08 +0100

    Update ZmcmsUsersServiceProvider.php

--- src/ZmcmsUsersServiceProvider.php
+++ src/ZmcmsUsersServiceProvider.php
@@ -1,6 +1,7 @@
 <?php
 namespace Zmcms\Users;
 use Illuminate\Support\ServiceProvider;
+use View;
 class ZmcmsUsersServiceProvider extends ServiceProvider{
 
 	public function register(){
@@ -15,14 +16,16 @@ class ZmcmsUsersServiceProvider extends ServiceProvider{
 		$this->loadRoutesFrom(__DIR__.DIRECTORY_SEPARATOR.'frontend\routes'.DIRECTORY_SEPARATOR.'users.php');
 		$this->loadMigrationsFrom(__DIR__.'/migrations');
 		$this->publishes([
-			__DIR__.'/config' => base_path('config/'.Config('frontend.theme_name').'/zmcms/users'),
-			__DIR__.'/backend/css' => base_path('public/themes/'.Config('frontend.theme_name').'/backend/css/'),
-			__DIR__.'/backend/js' => base_path('public/themes/'.Config('frontend.theme_name').'/backend/js/'),
-			__DIR__.'/backend/views' => base_path('resources/views/themes/'.Config('frontend.theme_name').'/backend'),
-			__DIR__.'/frontend/css' => base_path('public/themes/'.Config('frontend.theme_name').'/frontend/css/'),
-			__DIR__.'/frontend/js' => base_path('public/themes/'.Config('frontend.theme_name').'/frontend/js/'),
-			__DIR__.'/frontend/views' => base_path('resources/views/themes/'.Config('frontend.theme_name').'/frontendusers'),
+			__DIR__.'/backend/views' => base_path('resources/views/themes/zmcms/backend'),
+			__DIR__.'/frontend/views' => base_path('resources/views/themes/zmcms/frontend'),
+			__DIR__.'/config' => base_path('config/zmcms'),
+			__DIR__.'/backend/css' => base_path('public/themes/zmcms/backend/css'),
+			__DIR__.'/frontend/css' => base_path('public/themes/zmcms/frontend/css'),
+			__DIR__.'/backend/js' => base_path('public/themes/zmcms/backend/js'),
+			__DIR__.'/frontend/js' => base_path('public/themes/zmcms/frontend/js'),
 		]);
+		View::addLocation(__DIR__.DIRECTORY_SEPARATOR.'/backend/views');
+		View::addLocation(__DIR__.DIRECTORY_SEPARATOR.'/frontend/views');
 	}
 
 }
